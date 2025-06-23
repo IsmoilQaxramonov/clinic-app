508970cba52cc01bbaa479bc1cb2ee5043e769b4
IsmoilQaxramonov <frontqaxramonov@gmail.com>
2025-06-23 21:39:16 +0500

    added navbar and footer

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
+
 const App = () => {
-	return <h1 className="text-4xl font-bold">Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
